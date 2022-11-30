778ba74f0513edfb003b027c039979e7eb70f424
Lars <lars.riehle@outlook.de>
2022-11-30 15:39:37 +0100

    praise the lord

--- scripts/script.js
+++ scripts/script.js
@@ -11,40 +11,34 @@ const people_italien = 59070000;
 const italien = "Italien";
 const people_2003_italien = 57310000;
 const people_eu = 447700000;
-function germany() {
-    document.querySelector("#people").innerHTML = (people_deutschland / 1000000) + "Mio";
-    document.querySelector("#text").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Deustchalnd in 2022";
-    document.querySelector("#relativ").innerHTML = (people_deutschland / people_eu * 100) + "%";
-    document.querySelector("#rate").innerHTML = ((people_deutschland / people_2003_deutschland - 1) * 100) + "%";
-    document.querySelector("#people_03").innerHTML = ((people_deutschland - people_2003_deutschland) / 1000000) + "Mio";
-    document.querySelector(".chart").style.height = (people_deutschland / people_eu * 100) + "%";
+var country;
+var pp;
+var pp03;
+function normal(country, pp, pp03) {
+    document.querySelector("#people").innerHTML = (pp / 1000000).toFixed(1) + "Mio";
+    document.querySelector("#text").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in " + country + " in 2022";
+    document.querySelector("#relativ").innerHTML = (pp / people_eu * 100).toFixed(1) + "%";
+    document.querySelector("#rate").innerHTML = ((pp / pp03 - 1) * 100).toFixed(2) + "%";
+    document.querySelector("#people_03").innerHTML = ((pp - pp03) / 1000000).toFixed(2) + "Mio";
+    document.querySelector(".chart").style.height = (pp / people_eu * 100) + "%";
+    console.log(pp);
+    console.log(pp03);
+    console.log(country);
 }
-document.querySelector(".germany").addEventListener(`click`, germany);
-function france() {
-    document.querySelector("#people").innerHTML = (people_frankreich / 1000000) + "Mio";
-    document.querySelector("#text").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Frankreich in 2022";
-    document.querySelector("#relativ").innerHTML = (people_frankreich / people_eu * 100) + "%";
-    document.querySelector("#rate").innerHTML = ((people_frankreich / people_2003_frankreich - 1) * 100) + "%";
-    document.querySelector("#people_03").innerHTML = ((people_frankreich - people_2003_frankreich) / 1000000) + "Mio";
-    document.querySelector(".chart").style.height = (people_frankreich / people_eu * 100) + "%";
-}
-document.querySelector(".france").addEventListener(`click`, france);
-function austria() {
-    document.querySelector("#people").innerHTML = (people_oesterreich / 1000000) + "Mio";
-    document.querySelector("#text").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Österreich in 2022";
-    document.querySelector("#relativ").innerHTML = (people_oesterreich / people_eu * 100) + "%";
-    document.querySelector("#rate").innerHTML = ((people_oesterreich / people_2003_oesterreich - 1) * 100) + "%";
-    document.querySelector("#people_03").innerHTML = ((people_oesterreich - people_2003_oesterreich) / 1000000) + "Mio";
-    document.querySelector(".chart").style.height = (people_oesterreich / people_eu * 100) + "%";
-}
-document.querySelector(".austria").addEventListener(`click`, austria);
-function italy() {
-    document.querySelector("#people").innerHTML = (people_italien / 1000000) + "Mio";
-    document.querySelector("#text").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Italien in 2022";
-    document.querySelector("#relativ").innerHTML = (people_italien / people_eu * 100) + "%";
-    document.querySelector("#rate").innerHTML = ((people_italien / people_2003_italien - 1) * 100) + "%";
-    document.querySelector("#people_03").innerHTML = ((people_italien - people_2003_italien) / 1000000) + "Mio";
-    document.querySelector(".chart").style.height = (people_italien / people_eu * 100) + "%";
-}
-document.querySelector(".italy").addEventListener(`click`, italy);
+//normal("Deutschland", people_deutschland, people_2003_deutschland);
+//normal("Italien", people_italien, people_2003_italien);
+//normal("Österreich", people_oesterreich, people_2003_oesterreich);
+//normal("Frankreich", people_frankreich, people_2003_frankreich)
+document.querySelector(".germany").addEventListener(`click`, function () {
+    normal("Deustchland", people_deutschland, people_2003_deutschland);
+});
+document.querySelector(".italy").addEventListener(`click`, function () {
+    normal("ITALIEN", people_italien, people_2003_italien);
+});
+document.querySelector(".austria").addEventListener(`click`, function () {
+    normal("Österreich", people_oesterreich, people_2003_oesterreich);
+});
+document.querySelector(".france").addEventListener(`click`, function () {
+    normal("Frankreich", people_frankreich, people_2003_frankreich);
+});
 //# sourceMappingURL=script.js.map
--- scripts/script.ts
+++ scripts/script.ts
@@ -12,42 +12,37 @@ const italien = "Italien";
 const people_2003_italien = 57310000;
 const people_eu = 447700000;
 
-function germany(){
-    document.querySelector("#people").innerHTML = (people_deutschland / 1000000) + "Mio";
-    document.querySelector("#text").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Deustchalnd in 2022"
-    document.querySelector("#relativ").innerHTML = ( people_deutschland/ people_eu * 100) + "%";
-    document.querySelector("#rate").innerHTML = (( people_deutschland/ people_2003_deutschland - 1) * 100) + "%";
-    document.querySelector("#people_03").innerHTML = ((people_deutschland - people_2003_deutschland) / 1000000) + "Mio";
-    (document.querySelector(".chart") as HTMLDivElement).style.height= (people_deutschland/ people_eu * 100)+"%";
-}
-document.querySelector(".germany").addEventListener(`click`, germany)
 
-function france(){
-    document.querySelector("#people").innerHTML = (people_frankreich / 1000000) + "Mio";
-    document.querySelector("#text").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Frankreich in 2022"
-    document.querySelector("#relativ").innerHTML = ( people_frankreich/ people_eu * 100) + "%";
-    document.querySelector("#rate").innerHTML = (( people_frankreich/ people_2003_frankreich - 1) * 100) + "%";
-    document.querySelector("#people_03").innerHTML = ((people_frankreich - people_2003_frankreich) / 1000000) + "Mio";
-    (document.querySelector(".chart") as HTMLDivElement).style.height= (people_frankreich/ people_eu * 100)+"%";
-}
-document.querySelector(".france").addEventListener(`click`, france)
 
-function austria(){
-    document.querySelector("#people").innerHTML = (people_oesterreich / 1000000) + "Mio";
-    document.querySelector("#text").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Österreich in 2022"
-    document.querySelector("#relativ").innerHTML = ( people_oesterreich/ people_eu * 100) + "%";
-    document.querySelector("#rate").innerHTML = (( people_oesterreich/ people_2003_oesterreich - 1) * 100) + "%";
-    document.querySelector("#people_03").innerHTML = ((people_oesterreich - people_2003_oesterreich) / 1000000) + "Mio";
-    (document.querySelector(".chart") as HTMLDivElement).style.height= (people_oesterreich/ people_eu * 100)+"%";
-}
-document.querySelector(".austria").addEventListener(`click`, austria)
 
-function italy(){
-    document.querySelector("#people").innerHTML = (people_italien / 1000000) + "Mio";
-    document.querySelector("#text").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Italien in 2022"
-    document.querySelector("#relativ").innerHTML = ( people_italien/ people_eu * 100) + "%";
-    document.querySelector("#rate").innerHTML = (( people_italien/ people_2003_italien - 1) * 100) + "%";
-    document.querySelector("#people_03").innerHTML = ((people_italien - people_2003_italien) / 1000000) + "Mio";
-    (document.querySelector(".chart") as HTMLDivElement).style.height= (people_italien/ people_eu * 100)+"%";
+var country: string;
+var pp: number;
+var pp03: number;
+
+function normal (country, pp, pp03): void {                 //was bringt mir void?
+    document.querySelector("#people").innerHTML = (pp / 1000000).toFixed(1) + "Mio";
+    document.querySelector("#text").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in " + country + " in 2022"
+    document.querySelector("#relativ").innerHTML = ( pp / people_eu * 100).toFixed(1) + "%";
+    document.querySelector("#rate").innerHTML = (( pp / pp03 - 1) * 100).toFixed(2) + "%";
+    document.querySelector("#people_03").innerHTML = ((pp - pp03) / 1000000).toFixed(2) + "Mio";
+    (document.querySelector(".chart") as HTMLDivElement).style.height= (pp / people_eu * 100)+"%";
+    console.log(pp);
+    console.log(pp03);
+    console.log(country);   
 }
-document.querySelector(".italy").addEventListener(`click`, italy)
+//normal("Deutschland", people_deutschland, people_2003_deutschland);
+//normal("Italien", people_italien, people_2003_italien);
+//normal("Österreich", people_oesterreich, people_2003_oesterreich);
+//normal("Frankreich", people_frankreich, people_2003_frankreich)
+document.querySelector(".germany").addEventListener(`click`, function(){
+    normal("Deustchland", people_deutschland, people_2003_deutschland);
+})
+document.querySelector(".italy").addEventListener(`click`, function(){
+    normal("ITALIEN", people_italien, people_2003_italien);
+})
+document.querySelector(".austria").addEventListener(`click`, function(){
+    normal("Österreich", people_oesterreich, people_2003_oesterreich);
+})
+document.querySelector(".france").addEventListener(`click`, function(){
+    normal("Frankreich", people_frankreich, people_2003_frankreich);
+})
